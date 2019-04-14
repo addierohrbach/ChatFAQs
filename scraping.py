ccfd63175c91bb98ca753cc3ff6e190f1bf77bb6
petersenea <Petersenea@sbcglobal.net>
2019-04-14 18:51:36 -0500

    scrapes data into multi-dem arrays, with some errors

--- scraping.py
+++ scraping.py
@@ -17,5 +17,38 @@ heading_box.pop()
 heading_box.pop()
 heading_box.pop()
 
-print(heading_box)
+# print(heading_box)
+# print(len(heading_box))
+# print(heading_box[0])
+multi_array = []
 
+for i in range(len(heading_box)):
+    print(f'----------{i}---------------')
+    html_section = heading_box[i]
+
+    id_name = heading_box[i].get('id')
+
+    starting_point = soup.find('h2',id=id_name)
+    print(f'starting point {starting_point}')
+    
+    if starting_point.next_sibling == None:
+        break
+    next_sib = starting_point.next_sibling.next_sibling
+    next_sib_child = ""
+
+    # print(f'------check {id_name}--------')
+    if len(next_sib.contents) > 0:  
+        next_sib_child = next_sib.contents[0]
+    array = []
+
+    
+    while next_sib_child != soup.find('a', href = '#top'):
+        array.append(next_sib)
+        next_sib = next_sib.next_sibling.next_sibling
+        if len(next_sib.contents) > 0:  
+            
+            next_sib_child = next_sib.contents[0]
+
+    # print(array)
+    multi_array.append(array)
+# print(multi_array)
