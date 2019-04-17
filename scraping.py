270f0db88362fb67bff65493529c05ef471785f0
petersenea <Petersenea@sbcglobal.net>
2019-04-17 12:57:20 -0500

    in the middle of debugging

--- scraping.py
+++ scraping.py
@@ -2,15 +2,19 @@
 from urllib.request import urlopen
 import bs4
 from bs4 import BeautifulSoup
+import codecs
 
 # specify the url
-website = 'https://myc.nm.org/MyChart/default.asp?mode=stdfile&option=faq#RE_enhancements'
+# website = 'https://myc.nm.org/MyChart/default.asp?mode=stdfile&option=faq'
 
 # query the website and return the html
-page = urlopen(website)
+# page = urlopen(website)
 
 # parse the html using beautiful soup
-soup = BeautifulSoup(page, 'html.parser')
+fixed = codecs.open("fixed.html",'r')
+soup = BeautifulSoup(fixed, 'html.parser')
+# soup = BeautifulSoup(page, 'lxml')
+print(soup.prettify())
 
 heading_box = soup.find_all('h2')
 # Deletes the last three elements in the array which are not relevant questions
@@ -25,18 +29,20 @@ multi_array = []
 error_arr = []
 
 for i in range(len(heading_box)):
-    # print(f'----------{i}---------------')
+    print(f'----------{i}---------------')
     html_section = heading_box[i]
 
     id_name = heading_box[i].get('id')
 
     starting_point = soup.find('h2',id=id_name)
-    # print(f'starting point {starting_point}')
+    print(f'starting point {starting_point}')
+    print(f'starting point.next_sib = {starting_point.next_sibling}')
     
-    if starting_point.next_sibling == None:
-        error_arr.append(id_name)
-        # print(f'-----error {id_name} ------ {error_arr}')
-        continue
+    # if there is no next sibling for some reason, move on to the next question
+    # if starting_point.next_sibling == None:
+    #     error_arr.append(id_name)
+    #     # print(f'-----error {id_name} ------ {error_arr}')
+    #     continue
     
     next_sib = starting_point.next_sibling.next_sibling
     next_sib_child = ""
@@ -49,23 +55,24 @@ for i in range(len(heading_box)):
     
     while next_sib_child != soup.find('a', href = '#top'):
         array.append(next_sib)
-        if next_sib.next_sibling == None:
-            error_arr.append(id_name)
-            # print(f'-----error {id_name} ------ {error_arr}')
-            break
-        if next_sib.next_sibling.next_sibling == None:
-            error_arr.append(id_name)
-            # print(f'-----error {id_name} ------ {error_arr}')
-            break
-        if type(next_sib) is not bs4.element.NavigableString:
-            error_arr.append(id_name)
-            # print(f'-----error {id_name} ------ {error_arr}')
-            break
+        # if next_sib.next_sibling == None:
+        #     error_arr.append(id_name)
+        #     # print(f'-----error {id_name} ------ {error_arr}')
+        #     break
+        # if next_sib.next_sibling.next_sibling == None:
+        #     error_arr.append(id_name)
+        #     # print(f'-----error {id_name} ------ {error_arr}')
+        #     break
+        # if type(next_sib) is not bs4.element.NavigableString:
+        #     error_arr.append(id_name)
+        #     # print(f'-----error {id_name} ------ {error_arr}')
+        #     break
         next_sib = next_sib.next_sibling.next_sibling
         if len(next_sib.contents) > 0:  
             
             next_sib_child = next_sib.contents[0]
-    # print(array)
+    print(array)
     multi_array.append(array)
 print(f"full array: {multi_array}")
 print(f"error_array: {error_arr}")
+
