ed65d70c2d283f8abf5f7a69cbb5c15305bda68b
addierohrbach <adelinerohrbach2020@u.northwestern.edu>
2019-05-08 15:37:42 -0500

    create new branch and route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_wtf.csrf import CSRFProtect
 
 import searchform
 import matching
+import similarity
 
 
 app = Flask(__name__)
@@ -23,6 +24,21 @@ def view_answer():
         print(f'-------------End of Matched Answer:-------------')
         return render_template("layout.html", form=form, answer=answer, matched_q=matched_q)
     return render_template("layout.html", form=form, answer="", matched_q="")
+
+@app.route("/similarity", methods=("GET", "POST"))
+def view_answer_similarity():
+    form = searchform.SearchForm()
+    if form.validate_on_submit():
+        print(f'-------------User Input:-------------')
+        print(form.question.data)
+        matched_q, answer = matching.predict(form.question.data)
+        print(f'-------------Matched Question:-------------')
+        print(matched_q)
+        print(f'-------------Matched Answer:-------------')
+        print(answer)
+        print(f'-------------End of Matched Answer:-------------')
+        return render_template("layout.html", form=form, answer=answer, matched_q=matched_q)
+    return render_template("layout.html", form=form, answer="", matched_q=""))
 
 
 if __name__ == "__main__":
