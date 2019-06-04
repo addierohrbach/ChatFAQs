b2650d1da5f7d0602b1caf7fcd8bc5efd3efcf5d
petersenea <Petersenea@sbcglobal.net>
2019-06-04 12:15:34 -0500

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -1,3 +1,4 @@
+"""
 import scraping
 import spacy
 
@@ -44,3 +45,107 @@ def predict(question):
 # print(scraping.answers[81])
 # print(scraping.answers[24])
 # print(scraping.answers[5])
+"""
+import scraping
+import spacy
+import numpy as np
+from bs4 import BeautifulSoup
+import copy
+
+
+nlp = spacy.load('en_core_web_md')
+
+qs = scraping.questions
+
+def fixqs(questions):
+    return "".join([str(x) for x in questions.contents])
+
+def calculate(question):
+    formatted_qs = [fixqs(q) for q in qs]
+    input_question = nlp(question)
+    similar_qs = []
+    for i in range(len(formatted_qs)):
+        q = nlp(formatted_qs[i])
+        similarity = input_question.similarity(q)
+        similar_qs.append([similarity, qs[i], scraping.answers[i]])
+    
+
+    return similar_qs
+
+
+def predict(question):
+    similar_qs = calculate(question)
+    similar_qs.sort(reverse=True)
+    if similar_qs[0][0] >= 0.9:
+        high_similar_qs = []
+        for que in similar_qs:
+            if que[0] <= 0.9:
+                break
+            high_similar_qs.append(que)
+        return high_similar_qs
+    first_sim = similar_qs[0][0]
+    last_sim = first_sim - 0.15
+    new_similar_qs = []
+
+    for quest in similar_qs:
+        if quest[0] <= last_sim or quest[0] < .5:
+            break
+        print(f'{quest[0]}')
+        print(f'{quest}')
+        new_similar_qs.append(quest)
+    
+    if len(new_similar_qs) == 0:
+        new_similar_qs.append([0, "<h2>Results not Found.</h2>", ['\n', "<p>Try rewording your question.</p>", '\n']])
+
+    return new_similar_qs
+
+def predictusinganswer(index, nquestion):
+    
+    
+    ans = scraping.answers[index]
+    ansstr = ''
+    for ele in ans:
+        if ele != '\n':
+            contents = ele.contents
+            print(f'contents: {contents}')
+            for content in contents:
+                # if content.string == None:
+                #     print(f'content: {content}')
+                #     contents2 = content.contents
+                #     for i in contents2:
+                #         ansstr += i.string
+                # else:
+                ansstr += content.string
+    predict1 = calculate(ansstr)
+    predict2 = calculate(nquestion)
+    
+    newprob = calculate(ansstr)
+    print(f'1: {len(predict1)}')
+    print(f'2: {len(predict2)}')
+
+    for i in range(len(predict1)):
+        newprob[i][0] = .2 *predict1[i][0] + (.8 * predict2[i][0])
+    newprob.pop(index)
+
+    #newprob = np.multiply(predict1, predict2)
+    newprob.sort(reverse=True)
+    return newprob[0:5]
+
+
+
+formatted_qs = [fixqs(q) for q in qs]
+'''
+for i in range(len(qs)):#//fixqs(q) for q in qs)        
+    # if formatted_qs[i] == "I already have the MyChart app on my phone - can I continue to use the MyChart app instead of MyNM?":
+        # print ('HELLLO', i)
+    if qs[i].get('id') == "MR_message":
+        print('HELLO', i)
+'''
+# print('pizza',formatted_qs[12])
+
+# print(predictusinganswer(81, 'what type of documents'))
+print(predictusinganswer(12, 'forgot my password'))
+# print(predictusinganswer(12, 'what''s that'))
+# print(predictusinganswer(21, 'My doctor isn''t in the menu'))
+
+
